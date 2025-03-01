a6b2758a0c8e1fd4dac3ad76d0edfebc4c281d3b
Ngayka <ngayka@gmail.com>
2025-03-01 21:31:23 +0200

    ready tests

--- app/test_main.py
+++ app/test_main.py
@@ -2,11 +2,11 @@ import pytest
 from app.main import get_coin_combination
 
 
-def test_if_cents_0_should_be_zero_list():
+def test_if_cents_0_should_be_zero_list() -> None:
     assert get_coin_combination(0) == [0, 0, 0, 0]
 
 
-def test_list_len_should_be_4():
+def test_list_len_should_be_4() -> None:
     coins = get_coin_combination(25)
     assert len(coins) == 4
 
@@ -17,5 +17,6 @@ def test_list_len_should_be_4():
     (17, [2, 1, 1, 0]),
     (50, [0, 0, 0, 2])
 ])
-def test_should_return_coins_of_the_different_types(cents, coins):
+def test_should_return_coins_of_the_different_types(
+        cents: int, coins: list) -> None:
     assert get_coin_combination(cents) == coins
